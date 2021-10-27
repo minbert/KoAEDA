516b354678511b0388596259d685cbcdce1fa42b
seokmin <soos0228@naver.com>
2021-10-27 20:30:47 +0900

    feat : Mecab을 사용하지 않고 띄어쓰기 단위로 추가

--- src/koaeda/AEDA.py
+++ src/koaeda/AEDA.py
@@ -4,29 +4,13 @@ from copy import deepcopy
 from konlpy.tag import Mecab
 
 
-def morphs_except_specialToken(text, morph_func, special_tokens):
-    text_list = text.split()
-    morph_list = []
-    for word in text_list:
-        if word in special_tokens:
-            morph_list.extend([word, " "])
-        else:
-            words = morph_func(word)
-            morph_list.extend([*words, " "])
-    morph_list.pop()  # delete last space
-    return morph_list
-
-
 class AEDA:
     def __init__(
         self,
-        special_tokens=["[SEP]", "[CLS]"],
         punc_ratio=0.3,
         random_punc=True,
         punc_list=[".", ",", ";", ":", "?", "!"],
     ):
-        self.tag = Mecab()
-        self.special_tokens = special_tokens
         self.punc_ratio = punc_ratio
         self.random_punc = random_punc
         self.punc_list = punc_list
@@ -34,36 +18,30 @@ class AEDA:
     def __call__(self, *args, **kwds):
         return self.aeda(*args, **kwds)
 
-    def aeda(self, text: str, add_special_tokens: List[str] = []) -> str:
-        special_tokens = deepcopy(self.special_tokens) + add_special_tokens
+    def aeda(self, text: str) -> str:
         punc_ratio = self.punc_ratio
 
-        morph_list = morphs_except_specialToken(text, self.tag.morphs, special_tokens)
+        morph_list = text.split()
 
-        aug_word_index_list = [
-            index for index in range(len(morph_list)) if morph_list[index] != " "
-        ]
         punc_num = (
-            random.randint(1, int(punc_ratio * len(aug_word_index_list) + 1))
+            random.randint(1, int(punc_ratio * len(morph_list) + 1))
             if self.random_punc
-            else int(punc_ratio * len(aug_word_index_list) + 1)
+            else int(punc_ratio * len(morph_list) + 1)
         )
 
-        aug_word_index_list = random.sample(aug_word_index_list, punc_num)
+        aug_word_index_list = random.sample(range(len(morph_list) + 1), punc_num)
 
-        for index in range(len(morph_list)):
-            if index in aug_word_index_list:
-                aug = " " + random.choice(self.punc_list) + " "
-                if random.random() < 0.5:
-                    morph_list.insert(index, aug)
-                else:
-                    morph_list.insert(index + 1, aug)
+        aug_word_index_list.sort(reverse=True)
 
-        return "".join(morph_list)
+        for index in aug_word_index_list:
+            morph_list.insert(index, random.choice(self.punc_list))
+
+        return " ".join(morph_list)
 
 
-text = "이 주변에 맛집이 어디 있나요? [SEP] 그 맛집은 맛있나요?"
+if __name__ == "__main__":
+    text = "이 주변에 맛집이 어디 있나요? [SEP] 그 맛집은 맛있나요?"
 
-aeda = AEDA()
-for i in range(10):
-    print(aeda(text))
+    aeda = AEDA(punc_ratio=1)
+    for i in range(10):
+        print(aeda(text))
