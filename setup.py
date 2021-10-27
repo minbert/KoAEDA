f05ec6f659fe196f76475af7fecd6800128cd400
seokmin <soos0228@naver.com>
2021-10-27 20:29:56 +0900

    chore : 버전업 => Mecab 제거

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="koaeda",
-    version="0.0.5",
+    version="0.0.6",
     license="MIT",
     author="decyma",
     author_email="soos3121@gmail.com",
